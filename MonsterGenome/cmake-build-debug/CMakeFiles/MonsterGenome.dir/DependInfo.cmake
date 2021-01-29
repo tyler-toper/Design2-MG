d5f17c400bdc7b9effd136bc79a7f689387afff9
Bryan Delgado <bryandelgado0910@gmail.com>
2021-01-29 16:52:47 -0500

    CMake
    CMake stuff

--- MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/DependInfo.cmake
+++ MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/DependInfo.cmake
@@ -4,19 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Design2-MG/MonsterGenome/Armor.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Armor.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Enemy.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Enemy.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Game.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Game.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/GameState.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/GameState.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Hero.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Hero.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/LoadAssets.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/LoadAssets.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Menu.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Menu.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Pause.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Pause.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Platforms.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Platforms.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Projectile.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Projectile.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Settings.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Settings.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/Weapon.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Weapon.cpp.obj"
-  "C:/Design2-MG/MonsterGenome/main.cpp" "C:/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/main.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Armor.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Armor.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Enemy.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Enemy.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Game.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Game.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/GameState.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/GameState.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Hero.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Hero.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/LoadAssets.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/LoadAssets.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Menu.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Menu.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Pause.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Pause.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Platforms.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Platforms.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Projectile.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Projectile.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Settings.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Settings.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/Weapon.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/Weapon.cpp.obj"
+  "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/main.cpp" "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/CMakeFiles/MonsterGenome.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- MonsterGenome/cmake-build-debug/cmake_install.cmake
+++ MonsterGenome/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Design2-MG/MonsterGenome
+# Install script for directory: D:/Documents/School/Design/MG/Design2-MG/MonsterGenome
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Design2-MG/MonsterGenome/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Documents/School/Design/MG/Design2-MG/MonsterGenome/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
